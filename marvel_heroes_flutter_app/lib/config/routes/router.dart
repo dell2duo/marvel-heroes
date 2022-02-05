4aed152c65a2bf03c52c33bf49ac2f23afa1be1c
dell2duo <rafael.rocha.ufv@gmail.com>
2022-02-05 15:02:53 -0300

    files & folders structure created, routes configured

---
+++ marvel_heroes_flutter_app/lib/config/routes/router.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:marvel_heroes_flutter_app/core/home/home.view.dart';
+import 'routes.constants.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case HomeViewPageRoute:
+      return MaterialPageRoute(builder: (_) => const HomeView());
+    default:
+      return MaterialPageRoute(
+        builder: (_) => Scaffold(
+          body: Center(
+            child: Text('No route defined for ${settings.name}'),
+          ),
+        ),
+      );
+  }
+}
